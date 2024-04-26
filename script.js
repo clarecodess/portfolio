a8112c56f3d5a12fb6a0e88f07ffea0ccaa3c964
Clare Codes <166509600+clarecodess@users.noreply.github.com>
2024-04-26 13:53:06 +0300

    project container is responsive

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ document.querySelector('.right-arrow').addEventListener('click', () => {
         behavior: 'smooth'
     })
 })
+
+// hamburger menu
+//select burger menu and navigation list
+const hamburger = document.getElementById('hamburger');
+const navList = document.querySelector('nav ul');
+
+//function to toggle menu visibility
+function toggleMenu(){
+    // check if menu has show class
+    if (navList.classList.contains('show')){
+        //if it does close the menu
+        navList.classList.remove('show');
+    } else {
+        // if it doesn't, open the menu
+        navList.classList.add('show');
+    }
+}
+
+//add event listener to hamburger button
+hamburger.addEventListener('click', toggleMenu);
